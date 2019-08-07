78581c8ec76d5d869b3ef3e31f71bee19363e1ed
cc <765618907@qq.com>
2019-08-08 00:23:07 +0800

    新增日历组件切换动画

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="calendar" :style="isDisp">
-    <div class="calendar-head" >
+    <div class="calendar-head">
       <span>{{ getCurrentTime }} <small>{{ getWeeks }}</small></span>
     </div>
-    <v-touch class="calendar-body" tag="ul" 
-    v-on:swipeleft="nextMonth" v-on:swiperight="preMonth">
-      <li>日</li>
-      <li>一</li>
-      <li>二</li>
-      <li>三</li>
-      <li>四</li>
-      <li>五</li>
-      <li>六</li>
-      <li v-for="occupy in getFirstDay">&nbsp;</li>
-      <li v-for="(d, index) in getMonthNum" :key="index" :class="{cur: d == day }">{{ d }}</li>
-    </v-touch>
+    <transition name="page-flip">
+      <v-touch class="calendar-body" v-show="show" tag="ul" v-on:swipeleft="nextMonth" v-on:swiperight="preMonth">
+        <li>日</li>
+        <li>一</li>
+        <li>二</li>
+        <li>三</li>
+        <li>四</li>
+        <li>五</li>
+        <li>六</li>
+        <li v-for="occupy in getFirstDay">&nbsp;</li>
+        <li v-for="(d, index) in getMonthNum" :key="index" :class="{cur: d == day }">{{ d }}</li>
+      </v-touch>
+    </transition>
   </div>
 </template>
 
 <script>
-  import {year,month,day,week,numberDay,firstDay} from '../assets/js/date.js';
+  import {
+    year,
+    month,
+    day,
+    week,
+    numberDay,
+    firstDay
+  } from '../assets/js/date.js';
 
   export default {
     name: '',
@@ -29,53 +37,66 @@
         month,
         day,
         week,
+        show: true
       }
     },
     computed: {
       //获取state
-      getStatus(){
+      getStatus() {
         return this.$store.state.calendarStatus;
       },
       //组件是否显示
-      isDisp(){
-        return this.getStatus ? { height:'240px' } : { height:0 } 
+      isDisp() {
+        return this.getStatus ? {
+          height: '240px'
+        } : {
+          height: 0
+        }
       },
-      getYear(){
+      getYear() {
         return this.year;
       },
-      getMonth(){
+      getMonth() {
         return this.month;
       },
       //获取当前时间
-      getCurrentTime(){
+      getCurrentTime() {
         return `${this.getYear} / ${this.getMonth} / ${day}`
       },
-      getWeeks(){
-        return week(this.year,this.month,this.day);
+      getWeeks() {
+        return week(this.year, this.month, this.day);
       },
       //获取当前月第一天是周几
-      getFirstDay(){
-        return firstDay(this.year,this.month)
+      getFirstDay() {
+        return firstDay(this.year, this.month)
       },
       //获取月天数
-      getMonthNum(){
-        return numberDay(this.month,this.day);
+      getMonthNum() {
+        return numberDay(this.month, this.day);
       }
     },
     methods: {
-      nextMonth(){
+      nextMonth() {
+        this.show = false;
         this.month++;
-        if(this.month > 12){
+        if (this.month > 12) {
           this.month = 1;
           this.year++;
         }
+        setTimeout(() => {
+          this.show = true;
+        }, 300);
       },
-      preMonth(){
+      preMonth() {
+        this.show = false;
         this.month--;
-        if(this.month < 1){
+        if (this.month < 1) {
           this.month = 12;
           this.year--;
         }
+        setTimeout(() => {
+          this.show = true;
+        }, 300);
       }
     },
   }
@@ -88,6 +109,7 @@
     color: white;
     transition: all 0.5s;
     overflow: hidden;
+    margin-top: 49px;
   }
 
   .calendar-head {
@@ -99,8 +121,7 @@
     list-style: none;
     padding-bottom: 5px;
     flex-flow: wrap;
-    flex: 7;
-    transition: all 1s;
+    /* transition: all .5s; */
   }
 
   .calendar-body>li {
@@ -116,11 +137,12 @@
     position: relative;
     z-index: 1;
   }
-  .cur::after{
+
+  .cur::after {
     content: ' ';
     width: 80%;
     height: 1.5em;
-    background-color: white; 
+    background-color: white;
     color: #409eff;
     position: absolute;
     left: 50%;
@@ -128,4 +150,5 @@
     margin-left: -40%;
     border-radius: 3px;
   }
+
 </style>
