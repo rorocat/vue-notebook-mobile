48e448f40fe2aeed41d7ffe80ce35a5ea565e0fd
cc <765618907@qq.com>
2019-08-09 17:50:38 +0800

    整理配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
   <div id="app">
-    
+    <transition name="page-flip">
+      <router-view />
+    </transition>
+    <help></help>
   </div>
 </template>
+<script>
+  import Home from './views/Home'
+  import Help from './components/Help.vue'
 
-<style>
+  export default {
+    components: {
+      Home,
+      Help
+    },
+  }
+</script>
+<style> 
+  @import './assets/css/base.css';
+
+  /* 过度开始前 */
+  .page-flip-enter {
+    /* 缩放 */
+    /* transform: scale(0); */
+
+    /* 翻转 */
+    transform: rotateY(180deg);
+
+    /* 平移 */
+    /* transform: translate(-100%); */
+  }
+  /* 过度时 */
+  .page-flip-enter-active {
+    transition: all .8s ease;
+  }
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  //配置项目根目录
+  publicPath: process.env.NODE_ENV === 'production' ? '/notebook/' :'/'
+  ,
   chainWebpack: config => {
     config.resolve.alias
-      .set("@src",'src')
-      .set('assets','src/assets')
+      .set("@src", 'src')
+      .set('assets', 'src/assets')
   }
 }
